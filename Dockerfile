64f6140b999fdc7092ad7206261cdd4d205bcf57
racanu <racanu@gmail.com>
2022-06-12 11:50:45 +0200

    Some attempts to share the git repository with the container

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,19 @@
 FROM alpine:3 AS system-base
 USER root
+
+ARG UID=1000
+ARG GID=1000
+ARG USER=racanu
+ARG GROUP=racanu
+
 ENV TZ=Europe/Amsterdam
 ENV PS1="[\\w]🐳 "
-RUN apk update && apk add git npm python3 python3-dev php nodejs
+RUN apk update && apk add vim git npm python3 python3-dev php nodejs
+#This is needed in order to share the git archive
+RUN addgroup -g ${GID} ${GROUP}
+#RUN mkdir /home/${USER}
+#RUN chown ${UID}:${GID} /home/${USER}
+RUN echo ${USER}:x:${UID}:${GID}:${USER}:/home/${USER}:/bin/ash >> /etc/passwd
+RUN adduser ${USER} root
+USER ${UID}:${GID}
+
--- shell.sh
+++ shell.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 SHELL_IMAGE=claim:1
 
-shopt -s nocasematch && [[ "$1" = "clean" ]] && echo Removing image $SHELL_IMAGE
-shopt -s nocasematch && [[ "$1" = "clean" ]] && docker image rm $SHELL_IMAGE
+shopt -s nocasematch && [[ "$1" = "rebuild" ]] && echo Removing image $SHELL_IMAGE
+shopt -s nocasematch && [[ "$1" = "rebuild" ]] && docker image rm $SHELL_IMAGE
 
 docker image inspect $SHELL_IMAGE &> /dev/null
 [[ $? -ne 0 ]] && docker build --target system-base -t $SHELL_IMAGE -f Dockerfile .
 
-VOLUMES="-v $HOME:$HOME"
+VOLUMES="-v $HOME:$HOME -v /mnt:/mnt"
+ENV="-e PATH=$PATH"
 
 grep -q docker /proc/1/cgroup
 [[ $? -eq 0 ]] && VOLUMES="--volumes-from `hostname`"
 
-docker run -it --rm $VOLUMES -w `pwd` $SHELL_IMAGE /bin/sh
+docker run -it --rm $VOLUMES --env PATH --workdir `pwd` $SHELL_IMAGE /bin/sh
